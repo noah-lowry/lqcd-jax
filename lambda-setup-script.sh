b7e60f1c7ab51c7b7fa24958e1c8c57e3d82700b
noah-lowry <noahclowry@gmail.com>
2025-03-01 22:48:42 +0000

    first commit

---
+++ lambda-setup-script.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+cd ~
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+sh Miniconda3-latest-Linux-aarch64.sh
+conda init
+exec
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
+
+conda create -n lattice-qcd Python=3.11 ipykernel jupyter pip
+conda activate lattice-qcd
+conda install numpy scipy matplotlib tqdm
+pip install -U "jax[cuda12]"
+pip install jax-tqdm
