7dbd44f215af7f42df8a63510bea76bf2836be83
noah-lowry <noahclowry@gmail.com>
2025-03-06 19:46:24 -0600

    redid a trial

--- lambda-setup-script.sh
+++ lambda-setup-script.sh
@@ -8,8 +8,8 @@ exec
 conda install -n base conda-libmamba-solver
 conda config --set solver libmamba
 
-conda create -n lattice-qcd Python=3.11 ipykernel jupyter pip
+conda create -n lattice-qcd Python=3.11 ipykernel jupyter pip -y
 conda activate lattice-qcd
-conda install numpy scipy matplotlib tqdm
+conda install numpy scipy matplotlib tqdm -y
 pip install -U "jax[cuda12]"
 pip install jax-tqdm
