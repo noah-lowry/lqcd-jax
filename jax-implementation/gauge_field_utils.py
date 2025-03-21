81daa7db758d32691b37bb465ada71d3bc2a8dc2
noah-lowry <noahclowry@gmail.com>
2025-03-21 12:58:56 -0500

    started fix to systematic hmc issue

--- jax-implementation/gauge_field_utils.py
+++ jax-implementation/gauge_field_utils.py
@@ -3,15 +3,83 @@ import numpy as np
 
 import jax
 import jax.numpy as jnp
-from sun_utils import SUGenerator
 
-gen = SUGenerator(3, module="jax", dtype=jnp.complex64)
-@jax.jit
-def coef_to_lie_group(coef):
-    coef = jax.lax.complex(coef, jnp.zeros_like(coef))
-    su = jnp.einsum("...N,Nij->...ij", coef, gen.generators.astype(coef.dtype))
+def _make_generators(N):
+    def _alpha_index(n, m):
+        assert m < n
+        return n*n + 2*(m-n) - 2
+
+    def _beta_index(n, m):
+        assert m < n
+        return n*n + 2*(m-n) - 1
+
+    def _gamma_index(n):
+        return n*n - 2
+
+    result = np.empty((N*N-1, N, N), dtype=np.complex128)
+    for m in range(1, N+1):
+        for n in range(m+1, N+1):
+            gen = np.zeros((N, N), dtype=np.complex128)
+            gen[m-1, n-1] = gen[n-1, m-1] = 0.5
+            result[_alpha_index(n, m)] = gen
+
+            gen = np.zeros((N, N), dtype=np.complex128)
+            gen[m-1, n-1] = -0.5j
+            gen[n-1, m-1] = 0.5j
+            result[_beta_index(n, m)] = gen
+
+            gen = np.diag(np.array([1,]*(n-1)+[0,]*(N+1-n), dtype=np.complex128))
+            gen[n-1, n-1] = 1-n
+            gen /= np.sqrt(2*n*(n-1))
+            result[_gamma_index(n)] = gen
+    return result
+
+@partial(jax.jit, static_argnums=(1,))
+def LALG_SU_N(coef, N=3):
+    """Takes as input a float array `coef[..., N^2-1]` and returns complex array `SU[..., N, N]`.
+    The N^2-1 values in `coef` correspond to the coefficients of su(N) generators (e.g. N=3 corresponds to the Gell-Mann matrices divided by 2)\n
+    Lie Algebra to Lie Group Special Unitary N."""
+    
+    su = LA_SU_N(coef, N)
     SU = jax.scipy.linalg.expm(1j*su)
+
     return SU
+
+@partial(jax.jit, static_argnums=(1,))
+def LA_SU_N(coef, N=3):
+    """Takes as input a float (or real-complex) array `coef[..., N^2-1]` and returns complex array `su[..., N, N]` such that `expm(1j*su)` belongs to SU(N)."""
+    coef = coef if jnp.iscomplexobj(coef) else jax.lax.complex(coef, jnp.zeros_like(coef))
+    generators = _make_generators(N)
+    
+    su = jnp.einsum("...N,Nij->...ij", coef, generators)
+
+    return su
+
+@jax.jit
+def proj_SU3(arr):
+    D, V = jnp.linalg.eigh(arr.conj().mT @ arr)
+    result = arr @ ((V * (jnp.expand_dims(D, axis=-2)**-0.5)) @ V.conj().mT)
+    result = result * (jnp.expand_dims(jnp.linalg.det(result), axis=[-1, -2]) ** (-1/3))
+    return result
+
+@partial(jax.jit, static_argnums=(1,))
+def unitary_violation(arr, aggregate="mean"):
+    """Measure of how much `arr` violates unitarity. Computes |U^H @ U - I|, where || is the Frobenius norm.\n
+    `aggregate`: `None, "mean", "max", "rms"`
+    """
+
+    I = np.broadcast_to(np.eye(arr.shape[-1], dtype=arr.dtype), arr.shape)
+
+    violation = jnp.linalg.matrix_norm(arr.conj().mT @ arr - I, ord="fro")
+
+    aggregate_fn = {
+        None: lambda x: x,
+        "mean": jnp.mean,
+        "max": jnp.max,
+        "rms": lambda x: jnp.sqrt(jnp.mean(jnp.square(x)))
+    }
+    
+    return aggregate_fn[aggregate](violation)
 
 @jax.jit
 def wilson_action(field, beta):
@@ -39,8 +107,8 @@ def wilson_action(field, beta):
 @jax.jit
 def accurate_wilson_hamiltonian_error(q0, p0, q1, p1, beta):
 
-    field0 = coef_to_lie_group(q0)
-    field1 = coef_to_lie_group(q1)
+    field0 = LALG_SU_N(q0)
+    field1 = LALG_SU_N(q1)
 
     def plaquette(field, mu, nu):
         U_mu = field[..., mu, :, :]
@@ -150,15 +218,10 @@ def wilson_loops_range(field, R, T):
     result = sum(f_mn(n, m) for m, n in zip(*np.triu_indices(4, k=1))) / 6  # where m is the time dimension
     return result
 
+    
 @jax.jit
 def smear_HYP(field, alpha1=0.75, alpha2=0.6, alpha3=0.3):
-    
-    def proj_SU3(arr):
-        D, V = jnp.linalg.eigh(arr.conj().mT @ arr)
-        result = arr @ ((V * (jnp.expand_dims(D, axis=-2)**-0.5)) @ V.conj().mT)
-        result = result * (jnp.expand_dims(jnp.linalg.det(result), axis=[-1, -2]) ** (-1/3))
-        return result
-    
+
     def staple_func(f1, f2, d1, d2):
         
         S_plus = jnp.einsum(
--- jax-implementation/integrators.py
+++ jax-implementation/integrators.py
@@ -1,21 +1,23 @@
 import jax
+from gauge_field_utils import LALG_SU_N
 
 def int_MN4_takaishi_forcrand(q0, p0, F_func, tau, nfev_approx, theta=0.08398315262876693, rho=0.2539785108410595, lambd=0.6822365335719091, mu=-0.03230286765269967):
     eps = tau / (nfev_approx // 5)
 
     def scan_fn(carry, _):
+        T_operator = lambda q, p, coef: LALG_SU_N(coef * p) @ q
         q, p = carry
 
         p = p - theta * eps * F_func(q)
-        q = q + rho * eps * p
+        q = T_operator(q, p, rho * eps)
         p = p - lambd * eps * F_func(q)
-        q = q + mu * eps * p
+        q = T_operator(q, p, mu * eps)
         p = p - (1 - 2 * (lambd + theta)) * 0.5 * eps * F_func(q)
-        q = q + (1 - 2 * (mu + rho)) * eps * p
+        q = T_operator(q, p, (1 - 2 * (mu + rho)) * eps)
         p = p - (1 - 2 * (lambd + theta)) * 0.5 * eps * F_func(q)
-        q = q + mu * eps * p
+        q = T_operator(q, p, mu * eps)
         p = p - lambd * eps * F_func(q)
-        q = q + rho * eps * p
+        q = T_operator(q, p, rho * eps)
         p = p - theta * eps * F_func(q)
 
         return (q, p), None
@@ -32,13 +34,14 @@ def int_MN2_omelyan(q0, p0, F_func, tau, nfev_approx, lambd=0.1931833275037836):
     eps = tau / (nfev_approx // 2)
 
     def scan_fn(carry, _):
+        T_operator = lambda q, p, coef: LALG_SU_N(coef * p) @ q
         q, p = carry
         
-        q = q + lambd * eps * p
+        q = T_operator(q, p, lambd * eps)
         p = p - 0.5 * eps * F_func(q)
-        q = q + (1 - 2 * lambd) * eps * p
+        q = T_operator(q, p, (1 - 2 * lambd) * eps)
         p = p - 0.5 * eps * F_func(q)
-        q = q + lambd * eps * p
+        q = T_operator(q, p, lambd * eps)
 
         return (q, p), None
     
@@ -53,10 +56,13 @@ def int_MN2_omelyan(q0, p0, F_func, tau, nfev_approx, lambd=0.1931833275037836):
 def int_LF2(q0, p0, F_func, tau, nfev_approx):
     eps = tau / nfev_approx
 
+
     def scan_fn(carry, _):
+        T_operator = lambda q, p, coef: LALG_SU_N(coef * p) @ q
+
         q, p = carry
         p = p - 0.5 * eps * F_func(q)
-        q = q + eps * p
+        q = T_operator(q, p, eps)
         p = p - 0.5 * eps * F_func(q)
         return (q, p), None
     
